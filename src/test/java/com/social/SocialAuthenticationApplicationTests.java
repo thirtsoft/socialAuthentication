97af66b2fc3f71ece45c6fcf02503ffd52bbd66b
thirtsoft <thirtsoft@gmail.com>
2021-08-16 16:47:39 +0000

    first commit

---
+++ src/test/java/com/social/SocialAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
